72a94ccf5a09829898e658ec5ca6c9e7cabeb488
Ace <ace.github@gmail.com>
2014-06-01 17:27:10 -0700

    Basic user input program

---
+++ CPP/user_input.cpp
@@ -0,0 +1,24 @@
+// Program for getting user input
+// By Ace Eddleman
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input_var = 0;
+
+	do {
+		cout << "Enter a number (-1 = quit): ";
+
+		if (!(cin >> input_var)) {
+			cout << "You entered a non-numeric. Exiting..." << endl;
+			break;
+		}
+		if (input_var != -1) {
+			cout << "You entered " << input_var << endl;
+ 		}
+	
+	} while (input_var != -1);
+	cout << "All done." << endl;
+	return 0;
+}
