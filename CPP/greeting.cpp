11633b9f8d4d0d781838624d5eed73171590f62f
Ace <ace.github@gmail.com>
2014-06-04 17:09:12 -0700

    Modified greeting program to utilize loops

--- CPP/greeting.cpp
+++ CPP/greeting.cpp
@@ -9,6 +9,7 @@ int main() {
 	cout << "Please enter your first name: ";
 	string first_name;
 	cin >> first_name;
+
 	cout << "Please enter your last name: ";
 	string last_name;
 	cin >> last_name;
@@ -16,20 +17,45 @@ int main() {
 	// Build the welcome message
 	const string greeting = "Hello, " + first_name + " " + last_name + "!";
 
-	// Build the second and fourth lines of ouput
-	const string spaces(greeting.size(), ' ');
-	const string second = "* " + spaces + " *";
+	// Number of blanks surrounding the greeting
+	const int pad = 1;
 
-	// Build the first and last (fifth) lines of ouput
-	const string first(second.size(), '*');
+	// Number of rows to write
+	const int rows = pad * 2 + 3;
+	const string::size_type cols = greeting.size() + pad * 2 + 2;
 
-	// Finalized output
+	// Separate output from input
 	cout << endl;
-	cout << first << endl;
-	cout << second << endl;
-	cout << "* " << greeting << " *" << endl;
-	cout << second << endl;
-	cout << first << endl;
+
+	// Invariant: r rows have been written so far
+	for (int r = 0; r != rows; ++r) {
+		// Write a row of output
+		string::size_type c = 0;
+
+		// Invariant: c characters have been written so far in the current row
+		while (c != cols) {
+
+			// Check to see if it is time to write the greeting
+			if (r == pad + 1 &&  c == pad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			}
+			else {
+
+				// Check if we are on the border
+				if (r == 0 || r == rows - 1 || 
+					c == 0 || c == cols - 1)
+					cout << "*";
+				else
+					cout << " ";
+				++c;
+			}
+		}
+		cout << endl;
+	}
+
+
+
 
 	return 0;
 }
