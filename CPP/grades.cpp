0296b294ca243bd533853ccc8dd7fb4c50725ee5
Ace <ace.github@gmail.com>
2014-06-11 16:43:45 -0700

    Refined grading program in C++

--- CPP/grades.cpp
+++ CPP/grades.cpp
@@ -6,28 +6,40 @@
 using namespace std;
 
 int main() {
+	// Ask for and read student's name
+	cout << "Please enter your name: ";
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
 
-	// Ask for and read student name
-	cout << "Enter your midterm and final exam grades: ";
+	// Ask for and read the midterm and final grades
+	cout << "Please enter your midterm and final exam grades: ";
 	double midterm, final;
 	cin >> midterm >> final;
 
-	// Ask for homework grades
+	// Ask for the homework grades
+	cout << "Enter all your homework grades, "
+			"followed by EOF: ";
+
+	// The number and sum of grades read up to this point
 	int count = 0;
 	double sum = 0;
 
-	// Variable to read into
+	// A variable into which to read
 	double x;
 
-	// Invariant: We have read count grades so far, and
-	// sum is the sum of the first count grades
+	// Invariant: We have read "count" grades so far,
+	// and 'sum' is the sum of the first 'count' grades
 	while (cin >> x) {
 		++count;
-		sum += x;
-	}
+		sum +=x;
+	} 
 
-// Write the result
-streamsize prec = cout.precision();
-cout;
+	// Write the result
+	streamsize prec = cout.precision();
+	cout << "Your final grade is " << setprecision(3)
+		 << 0.2 * midterm + 0.4 * final + 0.4 * sum / count
+		 << setprecision(prec) << endl;
 
+	return 0;
 }
