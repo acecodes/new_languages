67a21883c99a1ce4c7ea5b0f7e7daa7d8adce237
Ace <ace.github@gmail.com>
2015-02-10 14:53:29 -0800

    Loops and celsius function in Go

--- Go/main.go
+++ Go/main.go
@@ -7,10 +7,47 @@ import "fmt"
 var hw = "Hello world!" // Long syntax
 const x = "You can't change this variable!" // Constant
 
+// Setting multiple variables at once
+var (
+    a = 5
+    b = 15
+    c = 25
+)
+
 func main() {
     hw2 := "Hello world!" // Short syntax
     fmt.Println(hw2)
     fmt.Println(x)
     fmt.Println("1 + 1.5 =", 1 + 1.5) // Automatically converts to float
     fmt.Println(hw[0])
+    fmt.Println(a, b, c)
+    celsius()
+    loops()
+}
+
+func celsius() {
+    // Convert temperatures to Celsius
+    fmt.Println((72.0 - 32.0) * (5.0/9.0)) // 72 degrees F
+}
+
+func loops() {
+    // Explicit syntax
+    i := 1
+    for i <= 10 {
+        fmt.Print(i) // Use print to avoid newlines
+        if i % 2 == 0 {
+            fmt.Print(" - Even ")
+        } else {
+            fmt.Print(" - Odd ")
+        }
+        fmt.Println()
+        i++
+    }
+
+    fmt.Println()
+
+    // Compressed syntax
+    for j := 1; j <= 10; j++ {
+        fmt.Print(j) 
+    }
 }
