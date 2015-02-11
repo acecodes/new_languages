bc28153d7415d3671e45a163a51b603642b680a3
Ace <ace.github@gmail.com>
2015-02-11 12:05:18 -0800

    Fun with functions and closures in Go

--- Go/main.go
+++ Go/main.go
@@ -4,102 +4,153 @@ import "fmt"
 
 // Exploring the basics of Go
 
-var hw = "Hello world!" // Long syntax
+var hw = "Hello world!"                     // Long syntax
 const x = "You can't change this variable!" // Constant
 
 // Setting multiple variables at once
 var (
-    a = 5
-    b = 15
-    c = 25
+	a = 5
+	b = 15
+	c = 25
 )
 
 func main() {
-    hw2 := "Hello world!" // Short syntax
-    fmt.Println(hw2)
-    fmt.Println(x)
-    fmt.Println("1 + 1.5 =", 1 + 1.5) // Automatically converts to float
-    fmt.Println(hw[0])
-    fmt.Println(a, b, c)
-    celsius()
-    loops()
-    arrays()
-    maps()
+	hw2 := "Hello world!" // Short syntax
+	fmt.Println(hw2)
+	fmt.Println(x)
+	fmt.Println("1 + 1.5 =", 1+1.5) // Automatically converts to float
+	fmt.Println(hw[0])
+	fmt.Println(a, b, c)
+	fmt.Println("72 degrees F =", celsius(72.0), "Celsius")
+	loops()
+	arrays()
+	maps()
+	avgArray := []float64{1, 2, 3}
+	fmt.Println("Average of", avgArray, "=", averages(avgArray))
+	addArray := []int{1, 2, 3, 5, 7}
+	fmt.Println("Result of adding", addArray, "=", add(addArray...))        // Use an array slice
+	fmt.Println("Result of adding a bunch of args =", add(5, 5, 10, 2, 10)) // Use ints
+	closure()
+	next := evenGenerator()
+	fmt.Println(next()) // 0
+	fmt.Println(next()) // 2
+	fmt.Println(next()) // 4
 }
 
-func celsius() {
-    // Convert temperatures to Celsius
-    fmt.Println((72.0 - 32.0) * (5.0/9.0)) // 72 degrees F
+func celsius(c float64) float64 {
+	// Convert temperatures to Celsius
+	return (c - 32.0) * (5.0 / 9.0)
 }
 
 func loops() {
-    // Explicit syntax
-    i := 1
-    for i <= 10 {
-        fmt.Print(i) // Use print to avoid newlines
-        if i % 2 == 0 {
-            fmt.Print(" - Even ")
-        } else {
-            fmt.Print(" - Odd ")
-        }
-        fmt.Println()
-        i++
-    }
+	// Explicit syntax
+	i := 1
+	for i <= 10 {
+		fmt.Print(i) // Use print to avoid newlines
+		if i%2 == 0 {
+			fmt.Print(" - Even ")
+		} else {
+			fmt.Print(" - Odd ")
+		}
+		fmt.Println()
+		i++
+	}
 
-    fmt.Println()
+	fmt.Println()
 
-    // Compressed syntax
-    for j := 1; j <= 10; j++ {
-        fmt.Print(j) 
-    }
+	// Compressed syntax
+	for j := 1; j <= 10; j++ {
+		fmt.Print(j)
+	}
 
-    fmt.Println()
+	fmt.Println()
 
-    // Using switch to selectively print English
-    for k := 1; k <= 10; k++ {
-        switch k {
-        case 2: fmt.Println("Two")
-        case 4: fmt.Println("Four")
-        }
-    }
+	// Using switch to selectively print English
+	for k := 1; k <= 10; k++ {
+		switch k {
+		case 2:
+			fmt.Println("Two")
+		case 4:
+			fmt.Println("Four")
+		}
+	}
 }
 
 func arrays() {
-    // Experimenting with arrays
-    var array1 [10]int
-    array1[0] = 1
-    array1[5] = 40
-    fmt.Println("My array: ", array1)
-    fmt.Println("My array's size: ", len(array1))
+	// Experimenting with arrays
+	var array1 [10]int
+	array1[0] = 1
+	array1[5] = 40
+	fmt.Println("My array: ", array1)
+	fmt.Println("My array's size: ", len(array1))
 
-    // Using the array shorthand
-    array2 := [5]float64{
-        1,
-        5,
-        92,
-        105,
-        3,
-    }
+	// Using the array shorthand
+	array2 := [5]float64{
+		1,
+		5,
+		92,
+		105,
+		3,
+	}
 
-    slice1 := array2[0:2]
-    slice2 := append(slice1, 4)
-    fmt.Println(slice1, slice2)
+	slice1 := array2[0:2]
+	slice2 := append(slice1, 4)
+	fmt.Println(slice1, slice2)
 
-    // Print the elements of the array
-    for i := 0; i < 10; i++ {
-        fmt.Print(array1[i])
-    }
+	// Print the elements of the array
+	for i := 0; i < 10; i++ {
+		fmt.Print(array1[i])
+	}
 
-    fmt.Println()
+	fmt.Println()
 }
 
 func maps() {
-    map1 := make(map[string]int)
-    map1["key"] = 10
-    fmt.Println(map1["key"])
-    map1["boom"] = 5
-    fmt.Println(map1["boom"])
-    fmt.Println(map1)
-    delete(map1, "boom")
-    fmt.Println(map1)
+	map1 := make(map[string]int)
+	map1["key"] = 10
+	fmt.Println(map1["key"])
+	map1["boom"] = 5
+	fmt.Println(map1["boom"])
+	fmt.Println(map1)
+	delete(map1, "boom")
+	fmt.Println(map1)
+}
+
+func averages(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
+}
+
+func add(args ...int) int {
+	// Adds all numbers specified
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
+}
+
+func closure() {
+	// Increments a number each time it is called
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment()) // 1
+	fmt.Println(increment()) // 2
+	fmt.Println(increment()) // 3
+}
+
+func evenGenerator() func() uint {
+	// Even number generator
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
 }
