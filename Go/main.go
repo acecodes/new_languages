f973562232e741dafefbc86825c00747d3222b0d
Ace <ace.github@gmail.com>
2015-02-17 22:21:36 -0800

    All kinds of experimentation with Go

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 import "math"
+import "math/rand"
+import "time"
 
 // Exploring the basics of Go
 
@@ -43,8 +45,8 @@ type Shape interface {
 	area() float64
 }
 
-// Function that uses the interface
 func totalArea(shapes ...Shape) float64 {
+	// Function that uses the interface
 	var area float64
 	for _, s := range shapes {
 		area += s.area()
@@ -52,6 +54,17 @@ func totalArea(shapes ...Shape) float64 {
 	return area
 }
 
+// Concurrency
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		// Random delays to show concurrency
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+// Misc functions
 func celsius(c float64) float64 {
 	// Convert temperatures to Celsius
 	return (c - 32.0) * (5.0 / 9.0)
@@ -194,46 +207,112 @@ func (c *Circle) area() float64 {
 	return math.Pi * c.r * c.r
 }
 
+// Channels
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+// Experimentation space - uncomment lines of interest
 func main() {
-	hw2 := "Hello world!" // Short syntax
-	fmt.Println(hw2)
-	fmt.Println(x)
-	fmt.Println("1 + 1.5 =", 1+1.5) // Automatically converts to float
-	fmt.Println(hw[0])
-	fmt.Println(a, b, c)
-	fmt.Println("72 degrees F =", celsius(72.0), "Celsius")
-	loops()
-	arrays()
-	maps()
-	avgArray := []float64{1, 2, 3}
-	fmt.Println("Average of", avgArray, "=", averages(avgArray))
-	addArray := []int{1, 2, 3, 5, 7}
-	fmt.Println("Result of adding", addArray, "=", add(addArray...))        // Use an array slice
-	fmt.Println("Result of adding a bunch of args =", add(5, 5, 10, 2, 10)) // Use ints
-	closure()
-	next := evenGenerator() // Create instance of generator
-	fmt.Println(next())     // 0
-	fmt.Println(next())     // 2
-	fmt.Println(next())     // 4
-	defer last()            // Will not run until the main() exits
-	first()
-	x := 5
-	one(&x)
-	fmt.Println("Converted 5 to", x, "using pointers")
-	var c Circle // Instance of Circle with zeros in each field
-	fmt.Println(c)
-	c.x = 5
-	c.y = 5
-	c.r = 9
-	d := Circle{x: 0, y: 5, r: 2} // Instance of Circle with defined field values
-	fmt.Println(d)
-	fmt.Println("Area of Circle d =", circleArea(d)) // Using standalone function
-	fmt.Println("Area of Circle c =", c.area())      // Using a method of the instance
-	a := new(Android)
-	a.Name = "Andy the Android"
-	b := new(Robot)
-	b.Person.Name = "Robby the Robot"
-	a.Talk()                   // Inherited Talk
-	b.Person.Talk()            // Composed Talk
-	fmt.Println(totalArea(&c)) // Call the interface method
+	// hw2 := "Hello world!" // Short syntax
+	// fmt.Println(hw2)
+	// fmt.Println(x)
+	// fmt.Println("1 + 1.5 =", 1+1.5) // Automatically converts to float
+	// fmt.Println(hw[0])
+	// fmt.Println(a, b, c)
+	// fmt.Println("72 degrees F =", celsius(72.0), "Celsius")
+	// loops()
+	// arrays()
+	// maps()
+	// avgArray := []float64{1, 2, 3}
+	// fmt.Println("Average of", avgArray, "=", averages(avgArray))
+	// addArray := []int{1, 2, 3, 5, 7}
+	// fmt.Println("Result of adding", addArray, "=", add(addArray...))        // Use an array slice
+	// fmt.Println("Result of adding a bunch of args =", add(5, 5, 10, 2, 10)) // Use ints
+	// closure()
+	// next := evenGenerator() // Create instance of generator
+	// fmt.Println(next())     // 0
+	// fmt.Println(next())     // 2
+	// fmt.Println(next())     // 4
+	// defer last()            // Will not run until the main() exits
+	// first()
+	// x := 5
+	// one(&x)
+	// fmt.Println("Converted 5 to", x, "using pointers")
+	// var c Circle // Instance of Circle with zeros in each field
+	// fmt.Println(c)
+	// c.x = 5
+	// c.y = 5
+	// c.r = 9
+	// d := Circle{x: 0, y: 5, r: 2} // Instance of Circle with defined field values
+	// fmt.Println(d)
+	// fmt.Println("Area of Circle d =", circleArea(d)) // Using standalone function
+	// fmt.Println("Area of Circle c =", c.area())      // Using a method of the instance
+	// a := new(Android)
+	// a.Name = "Andy the Android"
+	// b := new(Robot)
+	// b.Person.Name = "Robby the Robot"
+	// a.Talk()                   // Inherited Talk
+	// b.Person.Talk()            // Composed Talk
+	// fmt.Println(totalArea(&c)) // Call the interface method
+	// go f(5)
+	// for i := 0; i < 10; i++ { // Runs concurrent loops
+	// 	go f(i)
+	// }
+	// var input string
+	// fmt.Scanln(&input)
+
+	// Runs pinger and ponger concurrently
+	// var c chan string = make(chan string)
+	// go pinger(c)
+	// go ponger(c)
+	// go printer(c)
+
+	// var input string
+	// fmt.Scanln(&input)
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
 }
