8e1bd6c7f77e489c6bec111105c26ca0a72c7b4c
Ace <ace.github@gmail.com>
2015-03-03 12:05:33 -0800

    PAdding more directives and taking logic off of the views and into the controllers

--- JS/angular/app/scripts/controllers/about.js
+++ JS/angular/app/scripts/controllers/about.js
@@ -1,45 +1,53 @@
 'use strict';
-(function () {
-/**
- * @ngdoc function
- * @name jsApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the jsApp
- */
-var app = angular.module('aceApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+(function() {
+    /**
+     * @ngdoc function
+     * @name jsApp.controller:AboutCtrl
+     * @description
+     * # AboutCtrl
+     * Controller of the jsApp
+     */
+    var app = angular.module('aceApp')
+        .controller('AboutCtrl', function($scope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+        });
+
+    // Array objects to be iterated through
+    var testObjs = [{
+            name: 'Bob Object',
+            price: 2.50,
+            canPurchase: true,
+            soldOut: false
+        }, {
+            name: 'Steve Object',
+            price: 3.75,
+            canPurchase: false,
+            soldOut: true
+        }, {
+            name: 'Barbara Object',
+            price: 5.00,
+            canPurchase: true,
+            soldOut: false
+        }
+
     ];
-  });
 
-// Array objects to be iterated through
-var testObjs = [
-   {
-    name: 'Bob Object',
-    price: 2.50,
-    canPurchase: true,
-    soldOut: false
-   },
-   {
-    name: 'Steve Object',
-    price: 3.75,
-    canPurchase: false,
-    soldOut: true
-   },
-   {
-    name: 'Barbara Object',
-    price: 5.00,
-    canPurchase: true,
-    soldOut: false
-   }
+    app.controller('testController', function() {
+        this.testObjs = testObjs;
+    });
 
-];
+    app.controller('PanelController', function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
 
-app.controller('testController', function () {
-    this.testObjs = testObjs;
-});
 })();
