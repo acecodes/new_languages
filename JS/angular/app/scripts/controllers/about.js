15f8d33acaed2edb3d5d69a14f46026e27bc9567
Ace <ace.github@gmail.com>
2015-03-03 13:31:58 -0800

    Real-time form updating and posting in Angular

--- JS/angular/app/scripts/controllers/about.js
+++ JS/angular/app/scripts/controllers/about.js
@@ -16,28 +16,41 @@
             ];
         });
 
-    // Array objects to be iterated through
-    var testObjs = [{
-            name: 'Bob Object',
-            price: 2.50,
-            canPurchase: true,
-            soldOut: false
-        }, {
-            name: 'Steve Object',
-            price: 3.75,
-            canPurchase: false,
-            soldOut: true
-        }, {
-            name: 'Barbara Object',
-            price: 5.00,
-            canPurchase: true,
-            soldOut: false
-        }
-
-    ];
 
     app.controller('testController', function() {
-        this.testObjs = testObjs;
+        this.testObjs = [{
+                name: 'Bob Object',
+                price: 2.50,
+                canPurchase: true,
+                soldOut: false,
+                reviews: [{
+                    stars: 1,
+                    body: 'This object sucks!',
+                    author: 'dude@dude.com'
+                }]
+            }, {
+                name: 'Steve Object',
+                price: 3.75,
+                canPurchase: false,
+                soldOut: true,
+                reviews: [{
+                    stars: 3,
+                    body: 'This object is OK!',
+                    author: 'bro@dude.com'
+                }]
+            }, {
+                name: 'Barbara Object',
+                price: 5.00,
+                canPurchase: true,
+                soldOut: false,
+                reviews: [{
+                    stars: 5,
+                    body: 'This object rules!',
+                    author: 'bra@dude.com'
+                }]
+            }
+
+        ];
     });
 
     app.controller('PanelController', function() {
@@ -47,6 +60,14 @@
         };
         this.isSelected = function(checkTab) {
             return this.tab === checkTab;
+        };
+    });
+
+    app.controller('ReviewController', function() {
+        this.review = {};
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
         };
     });
 
