1fa648f49bd5bd086b68c94d4846d866c0d2d856
Ace <ace.github@gmail.com>
2015-04-10 13:31:51 -0700

    Exploring types and arrays in Rust

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -15,5 +15,25 @@ fn main() {
 
     var2 += 3;
     println!("{}", var2);
+
+    // Allow for declared variables without values
+    let abc;
+
+    abc = 2;
+    println!("{}", abc);
+
+    // Exploration of types and arrays
+
+    // Although the compiler normally picks up what type you want,
+    // it also allows explicit casting
+    let _a_float: f64 = 5.0;
+    let _s_ints_32: [i32; 5] = [1, 2, 3, 4, 5];
+    let _u_ints_32: [u32; 5] = [1, 2, 4, 4, 5];
+    let _float_32: [f32; 5] = [1.0, 2.0, 3.0, 4.0, 5.0];
+
+    // Renaming existing types (aliasing) - CamelCase must be used or you will get a warning!
+    type Float = f64;
+    // Use new type name
+    let _test_float: Float = 4.2;
 }
 
