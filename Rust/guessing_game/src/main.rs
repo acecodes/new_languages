a485bc91d1569cd1f41175051a54d542fd3afeb3
Ace <ace.github@gmail.com>
2015-05-17 18:35:27 -0700

    Refined the guessing game in Rust

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,8 +1,19 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    // Let's generate a random number
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // Print it out for testing purposes only
+    println!("The secret number is: {}", secret_number);
+
+    // Allow users to make a guess
     println!("Please input your guess:");
 
     let mut guess = String::new();
@@ -11,5 +22,27 @@ fn main() {
         .ok()
         .expect("Failed to read line");
 
+    // Typecasting guess into a 32-bit integer
+    let guess: u32 = guess.trim().parse()
+        .ok()
+        .expect("Please type a number.");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small."),
+        Ordering::Greater => println!("Too big."),
+        Ordering::Equal => println!("Justttt right."),
+    }
+
+    // // Testing out patterns
+    // let x = 7;
+
+    // match x {
+    //     1 | 2 => println!("One or two."),
+    //     3 => println!("Three."),
+    //     4 | 5 => println!("Four or five."),
+    //     _ => println!("Other.")
+    // }
+
 }
