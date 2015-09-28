ca079e150493046f434a85f5f9dfee420426caeb
Ace <ace.github@gmail.com>
2015-09-28 14:48:12 -0700

    More namedtuple experimentation in Python

--- Python/experiments.py
+++ Python/experiments.py
@@ -2,12 +2,12 @@ import collections
 from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
-
+Flashcards = collections.namedtuple('Flashcard', ['question', 'answer'])
 
 class CardDeck:
 
     """
-    A deck of cards
+    A deck of cards.
     """
     ranks = [str(n) for n in range(2, 11) + list('JQKA')]
     suits = ['spades', 'diamonds', 'clubs', 'hearts']
@@ -26,8 +26,35 @@ class CardDeck:
     def get_random(deck):
         return choice(deck)
 
+
+class FlashcardCollection:
+    """
+    A collection of simple Q&A flashcards.
+    """
+
+    def __init__(self):
+        self._flashcards = []
+
+    def __getitem__(self, position):
+        q = self._flashcards[position][0]
+        a = self._flashcards[position][1]
+        statement = 'Q: {}, A: {}'.format(q, a)
+        return statement
+
+    def __len__(self):
+        return len(self._flashcards)
+
+    def add_card(self, question, answer):
+        new_card = Flashcards(question, answer)
+        self._flashcards.append(new_card)
+
+
 if __name__ == '__main__':
     cards = CardDeck()
     print(cards[0])  # Testing __getitem__
     print(CardDeck.get_random(cards))
     print(Card('Q', 'spades') in cards)
+
+    flashcards = FlashcardCollection()
+    flashcards.add_card('What is 1+1?', '2')
+    print(flashcards[0])
